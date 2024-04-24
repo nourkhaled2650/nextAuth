a3e28eb9b089d4f494bf60ba380b4a5ab606b23c
Sonky <nourkhaled2650@gmail.com>
2024-04-25 01:39:33 +0200

    added font and finished landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <Button>click me</Button>
-    </div>
+    <main className=" font-Robotooo text-center flex h-full  flex-col items-center justify-center bg-gradient-to-b  from-slate-400 via-sky-700 to-slate-800">
+      <div className="space-y-6">
+        <h1 className="text-6xl text-white  drop-shadow-md">Auth</h1>
+        <p className="text-white text-lg">a single authentication service</p>
+        <Button> Sign in</Button>
+      </div>
+    </main>
   );
 }
